0327a7232b6bd80528c94f8ca77d3d476f6a9891
Clarissa Mobley <clarissamobley@me.com>
2024-04-09 18:48:22 -0400

    Combined index and drink HTML together. Moved CSS and JS over.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', function() {
     myModal.show();
 });
 
+
         //  date required for minimal age (21yrs old)
         let today = dayjs();
         const minAge = today.subtract(21, 'year').format('MM-DD-YYYY');
@@ -21,3 +22,41 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
         age.addEventListener("click", checkAge);
+
+        // For Happy Face
+for (let  i= 0;  i<=50; +i++) {
+    let happyFace = document.createElement('div');
+    happyFace.classList.add('happy');
+    let size = Math.random() * 15;
+    happyFace.style.fontSize = 3 + size + 'px';
+    happyFace.style.right = Math.random() * + innerWidth + 'px';
+    happyFace.style.top = Math.random() * + innerHeight + 'px';
+    document.querySelector('.background').appendChild(happyFace);
+}
+
+function animateHappy() {
+    let allHappy = document.querySelectorAll('.happy');
+    let num = Math.floor(Math.random()* allHappy.length);
+    allHappy[num].classList.toggle('animate');
+}
+
+// For Sad Face
+for(let i=0; i<=50; i++) {
+    let sadFace = document.createElement('div');
+    sadFace.classList.add('sad');
+    let size = Math.random() * 15;
+    sadFace.style.fontSize = 3 + size + 'px';
+    sadFace.style.right = Math.random() * + innerWidth + 'px';
+    sadFace.style.top = Math.random() * + innerHeight + 'px';
+    document.querySelector('.background').appendChild(sadFace);
+}
+
+
+function animateSad() {
+    let allSad = document.querySelectorAll('.sad');
+    let num2 = Math.floor(Math.random()*allSad.length);
+    allSad[num2].classList.toggle('animate');
+}
+
+setInterval(animateHappy, 50);
+setInterval(animateSad, 50);
