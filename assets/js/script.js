ca4cc7c9394aff9eecd843277fc5fdcd168319ef
eddie Hinojosa <eddie_hinojosa@live.com>
2024-04-08 21:59:08 -0400

    added minAge function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 
 //  date required for minimal age (21yrs old)
 
-// script that'll go in the index.html
-<script
-    src="https://cdn.jsdelivr.net/npm/dayjs@1.11.3/dayjs.min.js"
-    integrity="sha256-iu/zLUB+QgISXBLCW/mcDi/rnf4m4uEDO0wauy76x7U="
-    crossorigin="anonymous"
-    ></script> 
+const today = dayjs();
+const minAge = today.subtract(21, year).format (MM-DD-YYYY);
+let enteredAge = document.getElementById("age").value;
+let age = document.getElementById("age");  //replace with the id in the index.html
+
+// function that determintes if over or under 21yes of age
+function checkAge() {
+    if (enteredAge.isAfter(minAge)) {
+        window.alert("Under Age");
+    } else {
+        window.location.href = 'MoodSelection.html';
+    }
+}
+
+age.addEventListener("click", checkAge());
+
 
