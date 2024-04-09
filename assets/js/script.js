b01d5d51878183ae0e5ac984a7ca66df0b41e73b
eddie Hinojosa <eddie_hinojosa@live.com>
2024-04-08 23:18:46 -0400

    tsted teh function, edited HTML id

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@
 
 let today = dayjs();
 const minAge = today.subtract(21, 'year').format('MM-DD-YYYY');
-let age = document.getElementById('#btn'); 
+let age = document.getElementById('btn'); 
 
-// function that determintes if over or under 21yes of age
+
+// function that determintes if over or under 21 years of age
 function checkAge() {
-    let enteredAge = dayjs(document.getElementById('#btn').value, 'MM-DD-YYYY');
+    let enteredAge = dayjs(document.getElementById('ageSelector').value, 'MM-DD-YYYY');
     if (enteredAge.isAfter(minAge)) {
-        window.alert("Under Age");
+        window.alert('Under Age');
     } else {
         window.location.href = 'MoodSelection.html';
     }
