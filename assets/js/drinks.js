f9ce9e4186e7cc1fa5efade3a07b053b55830942
Clarissa Mobley <clarissamobley@me.com>
2024-04-09 14:01:25 -0400

    Added InLove Faces to JS. Fixed Bugs. Changed class name in CSS.

--- assets/js/drinks.js
+++ assets/js/drinks.js
@@ -1,18 +1,18 @@
 // For Happy Face
 for (let  i= 0;  i<=75; +i++) {
-    let exhaustedFace = document.createElement('div');
-    exhaustedFace.classList.add('happy');
+    let happyFace = document.createElement('div');
+    happyFace.classList.add('happy');
     let size = Math.random() * 15;
-    exhaustedFace.style.fontSize = 3 + size + 'px';
-    exhaustedFace.style.right = Math.random() * + innerWidth + 'px';
-    exhaustedFace.style.top = Math.random() * + innerHeight + 'px';
-    document.querySelector('.background').appendChild(exhaustedFace);
+    happyFace.style.fontSize = 3 + size + 'px';
+    happyFace.style.right = Math.random() * + innerWidth + 'px';
+    happyFace.style.top = Math.random() * + innerHeight + 'px';
+    document.querySelector('.background-faces').appendChild(happyFace);
 }
 
 function animateHappy() {
-    let allExhausted = document.querySelectorAll('.happy')
-    let num = Math.floor(Math.random()* allExhausted.length);
-    allExhausted[num].classList.toggle('animate');
+    let allHappy = document.querySelectorAll('.happy')
+    let num = Math.floor(Math.random()* allHappy.length);
+    allHappy[num].classList.toggle('animate');
 }
 
 // For Sad Face
@@ -23,7 +23,7 @@ for(let i=0; i<=75; i++) {
     sadFace.style.fontSize = 3 + size + 'px';
     sadFace.style.right = Math.random() * + innerWidth + 'px';
     sadFace.style.top = Math.random() * + innerHeight + 'px';
-    document.querySelector('.background').appendChild(sadFace);
+    document.querySelector('.background-faces').appendChild(sadFace);
 }
 
 
@@ -34,14 +34,14 @@ function animateSad() {
 }
 
 // For Angry Face
-for (let i=0; i <=50; i++) {
+for (let i=0; i <=75; i++) {
     let angryFace = document.createElement('div');
     angryFace.classList.add('angry');
     let size = Math.random() * 20;
     angryFace.style.fontSize = 3 + size + 'px';
     angryFace.style.right = Math.random() * + innerWidth + 'px';
     angryFace.style.top = Math.random() * + innerHeight + 'px';
-    document.querySelector('.background').appendChild(angryFace);
+    document.querySelector('.background-faces').appendChild(angryFace);
 }
 
 function animateAngry() {
@@ -51,14 +51,14 @@ function animateAngry() {
 }
 
 // For Exhausted Face
-for (let  i= 0;  i<=50; +i++) {
+for (let  i= 0;  i<=75; +i++) {
     let exhaustedFace = document.createElement('div');
     exhaustedFace.classList.add('exhausted');
     let size = Math.random() * 15;
     exhaustedFace.style.fontSize = 3 + size + 'px';
     exhaustedFace.style.right = Math.random() * + innerWidth + 'px';
     exhaustedFace.style.top = Math.random() * + innerHeight + 'px';
-    document.querySelector('.background').appendChild(exhaustedFace);
+    document.querySelector('.background-faces').appendChild(exhaustedFace);
 }
 
 function animateExhausted() {
@@ -67,10 +67,26 @@ function animateExhausted() {
     allExhausted[num].classList.toggle('animate');
 }
 
+// For In Love Face
+for (let  i= 0;  i<=75; +i++) {
+    let inLoveFace = document.createElement('div');
+    inLoveFace.classList.add('inLove');
+    let size = Math.random() * 15;
+    inLoveFace.style.fontSize = 3 + size + 'px';
+    inLoveFace.style.right = Math.random() * + innerWidth + 'px';
+    inLoveFace.style.top = Math.random() * + innerHeight + 'px';
+    document.querySelector('.background-faces').appendChild(inLoveFace);
+}
 
+function animateInLove() {
+    let allInLove = document.querySelectorAll('.inLove')
+    let num = Math.floor(Math.random()* allInLove.length);
+    allInLove[num].classList.toggle('animate');
+}
 
 
 // setInterval(animateHappy, 50);
 // setInterval(animateSad, 50);
 // setInterval(animateAngry, 50);
 // setInterval(animateExhausted, 50);
+// setInterval(animateInLove, 50);
