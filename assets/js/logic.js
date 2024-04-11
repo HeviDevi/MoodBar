11ea6a51c32acf093337a4babfcc067239f0a0ce
HeviDevi <devon.r.ross@gmail.com>
2024-04-11 15:02:49 -0400

    I've applied the same filtered API call method that I used for the Quote API to the CocktailDB API, now we just need to successfully gather user input and make some creative decisons regarding ingredient choices and quote categories

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,76 @@
+//// CocktailDB API MANIPULATION////
 
+function spiritParameter(){
+   
+   //sets spirit category as blank, to be altered by user input 
+    let spiritCategory = ''
+
+    //TODO: collect user input
+    let spiritInput = 'Gin'
+    
+  // Here the user's chosen input is assigned to a Spirit 
+  //We be adding this new value to the end of the URL in our ajax API call
+  //TODO: We need to look at our options for spirits in the API and assign them accordingly
+    if (spiritInput === 'Gin') {
+        spiritCategory = 'Gin'
+} else if (spiritInput === 'Rum') {
+    spiritCategory = 'Rum'
+} else if (spiritInput === 'Whiskey') {
+    spiritCategory = 'Whiskey'
+} else if (spiritInput === 'Vodka'){
+    spiritCategory = 'Vodka'
+} else if (spiritInput === 'Scotch') {
+    spiritCategory = 'Scotch'
+} else if (spiritInput === 'Tequila') {
+    spiritCategory = 'Tequila'
+} else if (spiritInup === 'Bourbon') {
+    spiritCategory = ' Bourbon'
+}
+
+//returns spiritCategory as output of function so we can use it later in the ajax api call
+return spiritCategory
+};
+
+function ingredientParameter(){
+    // sets category as blank
+    let ingredientCategory = ''
+    //TODO: Collect user's input
+    //TODO: Look over ingredients available in the API and make decisoons on what to leverage. we will have to be careful to format the new value correctly for the URL
+    let moodInput = 'Happy'
+
+    if (moodInput === 'Happy') {
+        ingredientCategory = 'Lemon_juice'
+    } else if (moodInput === 'Sad') {
+        ingredientCategory = 'Vermouth'
+    } else if (moodInput === 'Angry') {
+        ingredientCategory = 'Bitters'
+    } else if (moodInput === 'Exhausted') {
+       ingredientCategory = 'Orange'
+    } else if (moodInput === 'In Love') {
+        ingredientCategory = 'Sweet Vermouth'
+    }
+    return ingredientCategory
+};
+
+// Sets spiritCategory as the output of the spiritParameter function does likewise for the ingredientCategory ... or at least it should??
+let spiritCategory = spiritParameter();
+let ingredientCategory = ingredientParameter();
+// ajax api call to pull a quote from the api based on category
+//TODO// 
+//STYLE TEAM: create an HTML element to display the quote on the page.
+//STYLE TEAM: add "bartender's advice" button to drinks.html
+//CODE TEAM: event listener for quote button aaand a function that will display the quote on the page.. some kind of appendChild thing??
+$.ajax({
+    method: 'GET',
+    url: 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=' + ingredientCategory + ',' + spiritCategory,
+    success: function(result) {
+        ///Display function goes here
+        console.log(result.drinks[5].strDrink)
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
 
 
 
@@ -32,30 +104,33 @@ function quoteParameter(){
     //sets category as blank and then alters it based on user input
    
    //sets category as blank
-    let category = ''
+    let quoteCategory = ''
     //collcets chosen mood from index.html dropdown
-    let moodInput = document.getElementById('userMood').value;
+
+    // let moodInput = document.getElementById('userMood').value;
+    //TODO: Change this to the user input either my element Value or via local storage
+    let moodInput = 'Sad'
     // Here I have chosen various categories of quotes from the API and assigned them to our different moods
     //TODO: Look through available moods with group for final decisions on category assignment 
     // Happy = happiness, Sad = happiness, Angry=anger, Exhausted=future, In Love=love
     if (moodInput === 'Happy') {
-        category = 'happiness'
+        quoteCategory = 'happiness'
 } else if (moodInput === 'Sad') {
-    category = 'happiness'
+    quoteCategory = 'happiness'
 } else if (moodInput === 'Angry') {
-    category = 'anger'
+    quoteCategory = 'anger'
 } else if (moodInput === 'Exhausted'){
-    category = 'future'
+    quoteCategory = 'future'
 } else if (moodInput === 'In Love') {
-    category = 'love'
+    quoteCategory = 'love'
 }
 
-//returns category as output so we can use it later in the ajax api call
-return category
+//returns quoteCategory as output so we can use it later in the ajax api call
+return quoteCategory
 }
 
-// Sets category as the output of the quoteParameter function... or at least it should??
-let category = quoteParameter();
+// Sets quoteCategory as the output of the quoteParameter function... or at least it should??
+let quoteCategory = quoteParameter();
 
 // ajax api call to pull a quote from the api based on category
 //TODO// 
@@ -64,11 +139,12 @@ let category = quoteParameter();
 //CODE TEAM: event listener for quote button aaand a function that will display the quote on the page.. some kind of appendChild thing??
 $.ajax({
     method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + quoteCategory,
     headers: { 'X-Api-Key': 'lbuDF/d/4FN0Qtt3UdYKZw==0Y6n8GL3b39NEA5m'},
     contentType: 'application/json',
     success: function(result) {
         ///Display function goes here
+        console.log(result[0].author + 'once said' + '"' + result[0].quote + '"')
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
